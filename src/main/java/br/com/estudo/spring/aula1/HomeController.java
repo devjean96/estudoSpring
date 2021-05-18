3c28ac52fd5a52e08708a3d2bc4b659e056a73f5
Jean Moreira <devjean96@gmail.com>
2021-05-18 19:12:32 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/estudo/spring/aula1/HomeController.java
@@ -0,0 +1,15 @@
+package br.com.estudo.spring.aula1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homeApp(Model model){
+        model.addAttribute("mensagem", "mensagem foi adicionada");
+        return "index";
+    }
+}
