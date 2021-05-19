1a6d52676dc39e070ac22c641953a054e69a8f4a
Jean Moreira <devjean96@gmail.com>
2021-05-19 16:34:42 -0300

    aula 2 - controller e mapping get e post

---
+++ src/main/java/br/com/estudo/spring/aula1/EmpregoRepository.java
@@ -0,0 +1,8 @@
+package br.com.estudo.spring.aula1;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmpregoRepository extends CrudRepository<Emprego, Long> {
+
+
+}
--- src/main/java/br/com/estudo/spring/aula1/HomeController.java
+++ src/main/java/br/com/estudo/spring/aula1/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/inicio")
     public String homeApp(Model model){
         model.addAttribute("mensagem", "mensagem foi adicionada");
         return "index";
